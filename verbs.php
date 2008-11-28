f7ca59c4f02cb2dd58958e6eedb4e4b690d1a42d
Eric Warmenhoven <eric@warmenhoven.org>
2008-11-28 10:52:51 +0000

    don't know what it wasn't like this to begin with

--- verbs.php
+++ verbs.php
@@ -39,13 +39,13 @@
       principal parts for verbs from Chapter
       <select name="start">
 <? for ($i = 1; $i <= 40; $i++): ?>
-        <option value="<? echo $i ?>"><? echo $i ?></option>
+        <option value="<? echo $i; if ($i == $start) echo "\" selected=\"selected" ?>"><? echo $i ?></option>
 <? endfor ?>
       </select>
       to Chapter
       <select name="end">
 <? for ($i = 1; $i <= 40; $i++): ?>
-        <option value="<? echo $i; if ($i == 40) echo "\" selected=\"selected" ?>"><? echo $i ?></option>
+        <option value="<? echo $i; if ($i == $end) echo "\" selected=\"selected" ?>"><? echo $i ?></option>
 <? endfor ?>
       </select>
       <input type="submit" value="Quiz" />
@@ -57,53 +57,53 @@
     <table>
 
 <?
-	$count = 10;
-	$num = 0;
-	for ($i = 0; $i < sizeof($vocab); $i++) {
-		if ($start || $end) {
-			if ($end < $start) $end = $start;
-			if ($vocab[$i]['CHAPTER'] < $start)
-				continue;
-			if ($vocab[$i]['CHAPTER'] > $end)
-				continue;
-		}
-		if (strcasecmp($vocab[$i]['TYPE'], "verb"))
-			continue;
+  $count = 10;
+  $num = 0;
+  for ($i = 0; $i < sizeof($vocab); $i++) {
+    if ($start || $end) {
+      if ($end < $start) $end = $start;
+      if ($vocab[$i]['CHAPTER'] < $start)
+        continue;
+      if ($vocab[$i]['CHAPTER'] > $end)
+        continue;
+    }
+    if (strcasecmp($vocab[$i]['TYPE'], "verb"))
+      continue;
 
-		$list[$num++] = preg_split("/, /", $vocab[$i]['LATIN']);
-	}
+    $list[$num++] = preg_split("/, /", $vocab[$i]['LATIN']);
+  }
 
-	if (!sizeof($list)) {
-		print "<tr><td>There aren't any verbs to quiz you on!</td></tr>";
-	} else while ($count && sizeof($list)) {
-		$i = rand(0, sizeof($list) - 1);
-		if (!$part) {
-			// I like to focus on the last two parts
-			$j = rand(0, 6);
-			if ($j > 3) $j -= 2;
-			if ($j > 3) $j -= 2;
-		} else {
-			$j = $part - 1;
-		}
+  if (!sizeof($list)) {
+    print "<tr><td>There aren't any verbs to quiz you on!</td></tr>";
+  } else while ($count && sizeof($list)) {
+    $i = rand(0, sizeof($list) - 1);
+    if (!$part) {
+      // I like to focus on the last two parts
+      $j = rand(0, 6);
+      if ($j > 3) $j -= 2;
+      if ($j > 3) $j -= 2;
+    } else {
+      $j = $part - 1;
+    }
 
-		if (strcasecmp($list[$i][$j], "-")) {
+    if (strcasecmp($list[$i][$j], "-")) {
       print "<form onsubmit=\"return checkAnswer($count)\"><tr><td>";
-			for ($k = 0; $k < 4; $k++) {
-				if ($j == $k) {
-					$answer = $list[$i][$j];
+      for ($k = 0; $k < 4; $k++) {
+        if ($j == $k) {
+          $answer = $list[$i][$j];
           print "<input name=\"$answer\" id=\"$count\" />";
-				} else {
-					print $list[$i][$k];
-				}
-				if ($k != 3)
-					print ", ";
-			}
-			print "</td><td><label id=\"result$count\" /></td></tr></form>\n";
-			$count--;
-		}
+        } else {
+          print $list[$i][$k];
+        }
+        if ($k != 3)
+          print ", ";
+      }
+      print "</td><td><label id=\"result$count\" /></td></tr></form>\n";
+      $count--;
+    }
 
-		array_splice($list, $i, 1);
-	}
+    array_splice($list, $i, 1);
+  }
 ?>
     </table>
   </body>
